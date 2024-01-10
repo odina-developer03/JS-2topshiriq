1ebb9bf52d082a9ba4dff5a0f142a947ad0bc1ff
Пользователь <odinaabdurahimova03@gmail.com>
2024-01-10 22:09:29 +0500

    Added files

---
+++ main.js
@@ -0,0 +1,61 @@
+let sum =prompt ("1 dan 366 gacha bo`lgan sonlarni kiriting!")
+if((sum%7)==1 && sum<367){
+    console.log("Dushanba")
+}
+else if(sum==1){
+    console.log("Dushanba")
+}
+else if(sum==2){
+    console.log("Seshanba")
+}
+else if(sum==3){
+    console.log("Chorshanba")
+}
+else if(sum==4){
+    console.log("Payshanba")
+}
+else if(sum==5){
+    console.log("Juma")
+}
+else if(sum==6){
+    console.log("shanba")
+}
+else if((sum%7)==1 && sum<367){
+    console.log("Yakshanba")
+}
+else if((sum%7)==2 && sum<367){
+    console.log("Seshanba")
+}
+else if((sum%7)==3 && sum<367){
+    console.log("Chorshanba")
+}
+else if((sum%7)==4 && sum<367){
+    console.log("Payshanba")
+}
+else if((sum%7)==5 && sum<367){
+    console.log("Juma")
+}
+else if((sum%7)==6 && sum<367){
+    console.log("shanba")
+}
+else if((sum%7)==0 && sum<367){
+    console.log("yakshanba")
+}
+else {
+    console.log("Kechirasiz, 1 dan 366 gacha bolgan sonlarni kiriting!!!")
+}
+
+
+
+
+
+
+
+
+// let sum =prompt ("1 dan 366 gacha bo`lgan sonlarni kiriting!")
+// console.log(Math.floor(sum%7));
+
+
+
+
+// sum==1 && sum==7 && sum==14 && sum==28 && sum==35 && sum==42 && sum==49 && sum==56 && sum==63 && sum==70 && sum==77 && sum==84 && sum==91 && sum==98 && sum==105 && sum==112 && sum==119 && sum==126 && sum==133 && sum==140 && sum==147 && sum==154 && sum==161 && sum==168 && sum==175 && sum==182 && sum==189 && sum==196 && sum==203 && sum==210 && sum==217 && sum==224 && sum==231 && sum==238 
